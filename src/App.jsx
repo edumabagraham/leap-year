7b2535322e9f74f2c4b85d8c6b996cdda6d45a19
edumabagraham <edumabagrahamp@gmail.com>
2023-01-09 23:52:15 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [response, setResponse] = useState("")
+  const [formData, setFormData] = useState("")
+
+function handleChange(event){
+  setFormData(event.target.value)
+}
+
+function handleSubmit(event){
+  event.preventDefault()
+  checkYear(formData)
+}
+
+
+function checkYear(year){
+  if((year % 400 == 0) || 
+    ((year % 4 == 0) && 
+    (year % 100 != 0))){
+      setResponse("Yes!")
+      const firstTimeout = setTimeout(clearResponse,5000)
+    }
+  else 
+    setResponse("No!")
+    const secondTimeout = setTimeout(clearResponse,5000)
+}
+
+function clearResponse(){
+  setResponse("")
+}
+
+return (
+  <>
+  <h1 className="title">Leap Year?</h1>
+  <main>
+    <p className="subtitle">Enter a valid year.</p>
+    <form onSubmit={handleSubmit}>
+      <div className="input-field">
+        <input
+          type="number"
+          min="1000"
+          value = {formData}
+          name="year"
+          placeholder="Enter year"
+          onChange={handleChange}
+        />
+        <button>Go</button>
+      </div>
+      <p className="response">{response}</p>
+    </form>
+  </main>
+  </>
+)
+
+}
+
+export default App
